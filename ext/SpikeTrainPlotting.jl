256d36b5d1e0357b23f7b01ee40d2ee0390631ff
Dylan Festa <dylan.festa@gmail.com>
2023-10-13 13:57:13 +0200

    expand plotting

--- ext/SpikeTrainPlotting.jl
+++ ext/SpikeTrainPlotting.jl
@@ -38,4 +38,23 @@ function SpikeTrainUtilities.plot_spike_raster(trains::Vector{Vector{Float64}},
 end
 
 
+function SpikeTrainUtilities.plot_spike_raster(S::SpikeTrains{R,N},dt::Real,time_duration::Real;
+    t_start::Real=0.0,
+    spike_size::Integer = 5,
+    spike_separator::Integer = 1,
+    background_color::Color=RGB(1.,1.,1.),
+    spike_colors::Union{C,Vector{C}}=RGB(0.,0.0,0.0),
+    max_size::Real=1E4) where {R,N,C<:Color}
+  @assert t_start >= S.t_start "t_start is too small"   
+  @assert t_start + time_duration <= S.t_end "t_start + duration is too large"
+  trains = S.trains
+  return SpikeTrainUtilities.plot_spike_raster(trains,dt,time_duration;
+    t_start=t_start,
+    spike_size=spike_size,
+    spike_separator=spike_separator,
+    background_color=background_color,
+    spike_colors=spike_colors,
+    max_size=max_size)
+end
+
 end # module
